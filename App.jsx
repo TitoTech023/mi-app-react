089f13bde5cc8ceae4b00322cc61ea11d16d168e
TitoTech023 <managerpc36@gmail.com>
2026-07-06 13:27:06 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import AliadoForm from './components/AliadoForm'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <AliadoForm />
+    </div>
+  )
+}
+
+export default App
