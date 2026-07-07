510fbdbefeff063d05f904afc27aa1b7e92f8ba5
TitoTech023 <managerpc36@gmail.com>
2026-07-07 09:57:04 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'stonewall-luster-passport.ngrok-free.dev',
-      '*.ngrok-free.dev'  // Permite cualquier subdominio de ngrok
-    ]
+  root: '.',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
