c5d255bf800081613388e6ba7ecc558633c919ab
TitoTech023 <managerpc36@gmail.com>
2026-07-12 16:20:41 -0600

    es subir esta direccion https://mi-app-react-pb-production.up.railway.app

--- src/services/pocketbase.js
+++ src/services/pocketbase.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Conectar a PocketBase (cambia la URL cuando tengas el backend desplegado)
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://mi-app-react-pb-production.up.railway.app');
 
 // Obtener todos los aliados
 export const getAliados = async () => {
